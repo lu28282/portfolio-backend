7e7c1288793de4ce3c3e4b1d45ed76bbee4e5026
SenorZero <lucaschmitz@live.de>
2021-04-09 00:06:09 +0200

    add first ServiceImpl

---
+++ portfolio/src/main/java/com/portfolio/service/impl/assetServiceImpl.java
@@ -0,0 +1,29 @@
+package com.portfolio.service.impl;
+
+import java.util.List;
+
+import com.portfolio.dto.AssetDTO;
+import com.portfolio.service.interfaces.assetService;
+
+public class assetServiceImpl implements assetService {
+
+
+    @Override
+    public AssetDTO getAsset(int assetID) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public AssetDTO removeAsset(int assetID) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public List<AssetDTO> getAllAssets(int portfolioID) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+    
+}
